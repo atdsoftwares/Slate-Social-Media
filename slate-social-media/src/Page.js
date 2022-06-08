6da70dde5348d4c4f892205592ada82fd8673505
iprankurpandey <pprankur@gmail.com>
2022-06-08 17:45:49 +0530

    updated page structure

--- slate-social-media/src/Page.js
+++ slate-social-media/src/Page.js
@@ -1,7 +1,15 @@
 import React from "react";
+import App from "./App";
+import UserContext from "./Components/Context/UserContext";
 
 function Page() {
-  return <div></div>;
+  return (
+    <div>
+      <UserContext>
+        <App />
+      </UserContext>
+    </div>
+  );
 }
 
 export default Page;
