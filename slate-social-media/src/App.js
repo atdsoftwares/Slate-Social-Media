beb768f5b740701648b70f3a0d24979078b21c36
iprankurpandey <pprankur@gmail.com>
2022-06-08 12:10:34 +0530

    fix : updated folder structure

--- slate-social-media/src/App.js
+++ slate-social-media/src/App.js
@@ -1,9 +1,24 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Bookmarkpage from "./Pages/Bookmarkpage/Bookmarkpage";
+import Commentspage from "./Pages/Commentspage/Commentspage";
+import Explorepage from "./Pages/Explorepage/Explorepage";
+import Homepage from "./Pages/Homepage/Homepage";
+
+import Profielpage from "./Pages/Profilepage/Profielpage";
+import Welcomepage from "./Pages/Welcomepage/Welcomepage";
 
 function App() {
   return (
-    <div className="App">
-      <h1> THis is development branch</h1>
+    <div>
+      <Routes>
+        <Route path="/" element={<Welcomepage />} />
+        <Route path="/explore" element={<Explorepage />} />
+        <Route path="/profile" element={<Profielpage />} />
+        <Route path="/bookmark" element={<Bookmarkpage />} />
+        <Route path="/home" element={<Homepage />} />
+        <Route path="/comments" element={<Commentspage />} />
+      </Routes>
     </div>
   );
 }
