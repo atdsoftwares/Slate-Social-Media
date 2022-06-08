fd52da67d3b3a313e3cc80a6b9a3a70a7c25ca79
iprankurpandey <pprankur@gmail.com>
2022-06-08 17:45:39 +0530

    feat : can see all the users listed here

--- slate-social-media/src/Components/AccountSidebar/AccountSidebar.js
+++ slate-social-media/src/Components/AccountSidebar/AccountSidebar.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useUserContext } from "../Context/UserContext";
+import { getUsersFn } from "../Services/UserServices/Userservices";
 import "./AccountSidebar.css";
 function AccountSidebar() {
+  const { state, userDispatch } = useUserContext();
+  const { getUsers } = state;
+
+  useEffect(() => {
+    getUsersFn(userDispatch);
+  }, [userDispatch]);
+
   return (
     <div className="accounts-follow">
       <input
@@ -9,15 +18,13 @@ function AccountSidebar() {
         placeholder="search item"
       />
 
-      <div className="account-follow-details">
-        <img
-          src="https://lucent-media.netlify.app/static/media/mock.669f89742966b5eddb17.jpg"
-          alt="avatar"
-          className="avatar-img"
-        />
-        <h3 className="account-follow-details-text-name">Jaswant pandey</h3>
-        <span class="material-icons followicon">person_add</span>
-      </div>
+      {getUsers.map((user) => (
+        <div className="account-follow-details">
+          <img src={user.avatar} alt="avatar" className="avatar-img" />
+          <h3 className="account-follow-details-text-name">{user.fullName}</h3>
+          <span class="material-icons followicon">person_add</span>
+        </div>
+      ))}
       <hr />
     </div>
   );
