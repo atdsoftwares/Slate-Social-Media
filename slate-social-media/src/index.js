27a172543a9acb0cf4f25aae2f8add0c3c9d5109
iprankurpandey <pprankur@gmail.com>
2022-06-08 12:10:43 +0530

    updated folders

--- slate-social-media/src/index.js
+++ slate-social-media/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import Page from "./Page";
 import { makeServer } from "./server";
 
 // Call make Server
@@ -8,7 +10,9 @@ makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
