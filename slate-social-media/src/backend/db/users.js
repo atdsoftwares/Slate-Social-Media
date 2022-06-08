f6dd46422b05a0635d17dcae4e1608ab1f5c1339
iprankurpandey <pprankur@gmail.com>
2022-06-08 17:45:20 +0530

    added new users

--- slate-social-media/src/backend/db/users.js
+++ slate-social-media/src/backend/db/users.js
@@ -8,10 +8,49 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    username: "adarshbalika",
-    password: "adarshBalika123",
+    fullName: "Tanay Pratap",
+    username: "tanaypratap",
+    password: "tanaypratap123",
+    avatar:
+      "https://raw.githubusercontent.com/iprankurpandey/github-imagehosting/main/tanay.jpg",
+    bgImg:
+      "https://raw.githubusercontent.com/iprankurpandey/github-imagehosting/main/undraw_video_files_fu10.png",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    fullName: "Naman Saxena ",
+    username: "inamansaxena",
+    password: "namansaxena123",
+    avatar:
+      "https://raw.githubusercontent.com/iprankurpandey/github-imagehosting/main/naman.jpeg",
+    bgImg:
+      "https://raw.githubusercontent.com/iprankurpandey/github-imagehosting/main/undraw_social_networking_re_i1ex.svg",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    fullName: "Adarsh Patel",
+    username: "iadarshpatel",
+    password: "adarshpatel123",
+    avatar:
+      "https://raw.githubusercontent.com/iprankurpandey/github-imagehosting/main/adarshpateldev.jpeg",
+    bgImg:
+      "https://raw.githubusercontent.com/iprankurpandey/github-imagehosting/main/undraw_server_down_s4lk.png",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    fullName: "Prankur Pandey",
+    username: "iprankurpandey",
+    password: "prankurpandey123",
+    avatar:
+      "https://raw.githubusercontent.com/iprankurpandey/github-imagehosting/main/prankur.png",
+    bgImg:
+      "https://raw.githubusercontent.com/iprankurpandey/github-imagehosting/main/undraw_taking_notes_re_bnaf.svg",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
