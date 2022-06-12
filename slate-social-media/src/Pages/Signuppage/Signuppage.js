c88c5b6c81c2255c688d762ef58b0bdc136b816a
iprankurpandey <pprankur@gmail.com>
2022-06-12 22:42:36 +0530

    updated files

---
+++ slate-social-media/src/Pages/Signuppage/Signuppage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Signup from "../../Components/AuthForms/Signup";
+import Footer from "../../Components/Footer/Footer";
+import Header from "../../Components/Header/Header";
+
+function Signuppage() {
+  return (
+    <div>
+      <Header />
+      <Signup />
+      <Footer />
+    </div>
+  );
+}
+
+export default Signuppage;
