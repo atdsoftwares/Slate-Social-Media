f21496f236980bc492c68c4776f5846ccb276ed3
iprankurpandey <pprankur@gmail.com>
2022-06-12 22:42:24 +0530

    edtors

--- slate-social-media/src/Components/Post Model/PostModel.js
+++ slate-social-media/src/Components/Post Model/PostModel.js
@@ -1,35 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useComposePostContext } from "../Context/PostContext";
+import { useUserContext } from "../Context/UserContext";
+import { deletePostFn, editPostFn } from "../Services/Post/Postservices";
 import "./PostModel.css";
+function PostModel({ postdata }) {
+  const {
+    _id,
+    username,
+    content,
+    createdAt,
+    image,
+    video,
 
-function PostModel() {
+    // likes: { likeCount, likedBy, dislikedBy },
+    // updatedAt,
+  } = postdata;
+
+  const { state } = useUserContext();
+  const { getUsers } = state;
+  const { postDispatch } = useComposePostContext();
+  const userdata = getUsers.filter((u) => u.username === username);
+  const { avatar, fullName } = userdata[0];
+
+  // useEffect(() => {
+  //   getUsersFn(userDispatch);
+  // }, [userDispatch]);
+  const [display, setDisplay] = useState("none");
+
+  function toggleModal() {
+    setDisplay(display === "none" ? "block" : "none");
+  }
   return (
-    <div className="composed-post">
-      <div className="post-model">
-        <div className="post-model-data">
-          <Link to="/profile">
-            <div className="image-text">
-              <img
-                src="https://lucent-media.netlify.app/static/media/mock.669f89742966b5eddb17.jpg"
-                alt="avatar"
-                className="avatar-post-img"
-              />
-              <h3> Jaswant Pandey</h3>
+    <div className="post-container">
+      <div class="f-card">
+        <div class="header">
+          <div class="options">
+            <span class="material-icons postcardmi" onClick={toggleModal}>
+              more_vert
+            </span>
+            <div className="modal-container" style={{ display: display }}>
+              <Link to={`/edit/${_id}`}>
+                <span
+                  class="material-icons postcardmi"
+                  onClick={() => editPostFn(_id, image, video, content)}
+                >
+                  edit
+                </span>
+              </Link>
+              <span
+                class="material-icons postcardmi"
+                onClick={() => deletePostFn(postDispatch, _id)}
+              >
+                delete
+              </span>
             </div>
-          </Link>
-          <p className="text-post">
-            lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not.
-          </p>
+          </div>
+
+          <img class="co-logo" src={avatar} alt="user-avatar" />
+
+          <div class="co-name">
+            <span> {fullName}</span>
+            <div>{username}</div>
+          </div>
+          <div class="time">
+            <span href="#">Posted at : {createdAt}</span>
+          </div>
         </div>
-        <div className="actions-icons">
-          <span class="material-icons posticon">favorite_border</span>
-          <Link to="/comments">
-            <span class="material-icons posticon">chat_bubble_outline</span>
-          </Link>
-          <span class="material-icons posticon">bookmark_outline</span>
+        <div
+          class="content"
+          dangerouslySetInnerHTML={{ __html: content }}
+        ></div>
+
+        <div class="reference">
+          <div class="reference-content">
+            {image && (
+              <img class="reference-thumb" src={image} alt="uploaded-by-user" />
+            )}
+            {video && <video class="reference-video" src={video} controls />}
+          </div>
+        </div>
+        <div class="social">
+          <div class="social-buttons">
+            <span class="material-icons postcardmi">thumb_up_off_alt</span>
+            <Link to="/comments">
+              <span class="material-icons postcardmi">chat_bubble_outline</span>
+            </Link>
+            <span class="material-icons postcardmi">bookmark_border</span>
+          </div>
         </div>
       </div>
     </div>
--- slate-social-media/src/Components/Profileview/Profileview.js
+++ slate-social-media/src/Components/Profileview/Profileview.js
@@ -1,30 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLoginSignupContext } from "../Context/LoginSignupContext";
+import { useUserContext } from "../Context/UserContext";
+import { getUserDetailsFn, getUsersFn } from "../Services/User/Userservices";
 import "./Profileview.css";
 function Profileview() {
+  const { state, userDispatch } = useUserContext();
+  const { getUserDetails } = state;
+
+  const { loginData } = useLoginSignupContext();
+  const _id = loginData._id;
+
+  useEffect(() => {
+    getUserDetailsFn(userDispatch, _id);
+  }, []);
+
+  const { avatar, bgImg, fullName, followers, following, username } =
+    getUserDetails;
   return (
     <div className="composed-post">
-      <div className="post-model1">
-        <img
-          src="https://lucent-media.netlify.app/static/media/bg.493cd779825f711be2bf.jpeg"
-          alt="bgposter"
-          className="bg-poster-img"
-        />
-        <img
-          src="https://lucent-media.netlify.app/static/media/mock.669f89742966b5eddb17.jpg"
-          alt="avatar"
-          className="avatar-profile-img"
-        />
+      <div div className="post-model1">
+        <img src={bgImg} alt="bgposter" className="bg-poster-img" />
+        <img src={avatar} alt="avatar" className="avatar-profile-img" />
         <div className="btn-text-profile">
-          <h3 className="profile-user-name"> Jaswant Pandey</h3>
-          <sub> @iprankurpandey</sub>
+          <h3 className="profile-user-name"> {fullName}</h3>
+          <sub> @{username}</sub>
           <button className="btn btn-warning-outline bnt-follow">
             Follow
             <span class="material-icons profilemi">person_add</span>
           </button>
         </div>
         <div className="profile-counter">
-          <div className="profiler-followers"> follower : 10</div>
-          <div className="profiler-following"> following : 10</div>
+          <div className="profiler-followers"> Follower: {followers}</div>
+          <div className="profiler-following">Following: {following}</div>
           <div className="profiler-post"> posts : 10</div>
         </div>
       </div>
