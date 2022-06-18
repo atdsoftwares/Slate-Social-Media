3070bfbcd8f5fba2bb9cfb6572a66cee9c75a55f
iprankurpandey <pprankur@gmail.com>
2022-06-18 20:05:26 +0530

    fix : updated pages as per needs

--- slate-social-media/src/Pages/Profilepage/Profielpage.js
+++ slate-social-media/src/Pages/Profilepage/Profielpage.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "../../Components/Footer/Footer";
 import Header from "../../Components/Header/Header";
 import Profileview from "../../Components/Profileview/Profileview";
 import Sidebar from "../../Components/Sidebar/Sidebar";
 import PostModel from "../../Components/Post Model/PostModel";
+import { useCommentContext } from "../../Components/Context/CommentsContext";
+import { useUserContext } from "../../Components/Context/UserContext";
+import { getPostByUsernameFn } from "../../Components/Services/Post/Postservices";
+import { useComposePostContext } from "../../Components/Context/PostContext";
 function Profielpage() {
+  const { commentsDispatch } = useCommentContext();
+  const { getUserDetails } = useUserContext();
+  const { username } = getUserDetails;
+
+  const { getPostsByUserName, postDispatch } = useComposePostContext();
+  console.log(
+    "🚀 ~ file: Profielpage.js ~ line 17 ~ Profielpage ~ getPostsByUserName",
+    getPostsByUserName
+  );
+
+  useEffect(() => {
+    console.log(`I am being called `);
+    getPostByUsernameFn(postDispatch, username);
+  }, [postDispatch, username]);
   return (
     <div>
       <Header />
       <Sidebar />
       <Profileview />
+
+      {getPostsByUserName.map((postdata) => {
+        return <PostModel key={postdata._id} postdata={postdata} />;
+      })}
 
       <Footer />
     </div>
