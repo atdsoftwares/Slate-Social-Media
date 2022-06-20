b45a1dac359206d212c353eaba7a3de6b49b6c8f
iprankurpandey <pprankur@gmail.com>
2022-06-20 10:57:41 +0530

    fix :  follow unfollow fn

--- slate-social-media/src/Components/AccountSidebar/AccountSidebar.js
+++ slate-social-media/src/Components/AccountSidebar/AccountSidebar.js
@@ -1,15 +1,26 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { useLoginSignupContext } from "../Context/LoginSignupContext";
 import { useUserContext } from "../Context/UserContext";
-import { followUserFn } from "../Services/Follow-Unfollow/Follow-Unfollow-services";
+import {
+  followUserFn,
+  unFollowUserFn,
+} from "../Services/Follow-Unfollow/Follow-Unfollow-services";
 import { getUsersFn } from "../Services/User/Userservices";
 import "./AccountSidebar.css";
 function AccountSidebar() {
-  const { state, userDispatch } = useUserContext();
-  const { getUsers } = state;
+  const { getUsers, userDispatch, getUserDetails } = useUserContext();
+  console.log(
+    "🚀 ~ file: AccountSidebar.js ~ line 12 ~ AccountSidebar ~ getUsers",
+    getUsers
+  );
+
+  const { following, followers } = getUsers;
 
   const { loginData } = useLoginSignupContext();
-  const userId = loginData._id;
+
+  const userId = getUserDetails._id;
+
   // getuserdetail from usercontext id , send the saeme id
   useEffect(() => {
     getUsersFn(userDispatch);
@@ -25,15 +36,21 @@ function AccountSidebar() {
 
       {getUsers.map((user) => (
         <div className="account-follow-details" key={user._id}>
-          <img src={user.avatar} alt="avatar" className="avatar-img" />
+          <Link to={`/profile/${user._id}`}>
+            <img src={user.avatar} alt="avatar" className="avatar-img" />
+          </Link>
           <h3 className="account-follow-details-text-name">{user.fullName}</h3>
 
-          <span
-            class="material-icons followicon"
-            onClick={() => followUserFn(userDispatch, userId)}
-          >
-            person_add
-          </span>
+          {following?.find((userDetails) => userDetails._id === userId) ? (
+            <p> following</p>
+          ) : (
+            <span
+              class="material-icons followicon"
+              onClick={() => followUserFn(userDispatch, user._id)}
+            >
+              person_add
+            </span>
+          )}
         </div>
       ))}
       <hr />
