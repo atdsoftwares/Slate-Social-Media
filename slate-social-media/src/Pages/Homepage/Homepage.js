7a2616beda3b9225f95fedcb45abd1ae0b3982bb
iprankurpandey <pprankur@gmail.com>
2022-06-25 08:55:14 +0530

    working on new post editor

--- slate-social-media/src/Pages/Homepage/Homepage.js
+++ slate-social-media/src/Pages/Homepage/Homepage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AccountSidebar from "../../Components/AccountSidebar/AccountSidebar";
 import Posteditor from "../../Components/ComposePost Editor/PostEditor";
+import Editpost from "../../Components/editpost/Editpost";
 import Footer from "../../Components/Footer/Footer";
 import Header from "../../Components/Header/Header";
 import Sidebar from "../../Components/Sidebar/Sidebar";
@@ -11,6 +12,8 @@ function Homepage() {
       <Header />
       <Sidebar />
       <Posteditor />
+      {/* <Editpost /> */}
+
       <AccountSidebar />
       <Footer />
     </div>
