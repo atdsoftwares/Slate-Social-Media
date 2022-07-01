13b8557410b6690af0b2cfc7e235f3c76b78b9e7
iprankurpandey <pprankur@gmail.com>
2022-07-01 18:17:59 +0530

    added redux

--- slate-social-media/src/index.js
+++ slate-social-media/src/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import Page from "./Page";
-import { makeServer } from "./server";
+import App from "./App";
 
+import { makeServer } from "./server";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Page />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
