92dacd1353200e5b9ed4606b497090dd3674b62f
iprankurpandey <pprankur@gmail.com>
2022-07-01 18:19:01 +0530

    comment api call redux asyncthunk

---
+++ slate-social-media/src/redux/reducers/commentsSlice.js
@@ -0,0 +1,95 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  composeComment: [],
+  commentsdata: [],
+  commentBoxInput: "",
+};
+
+export const postCommentFn = createAsyncThunk(
+  "comments/composeComment",
+  async (data) => {
+    try {
+      const response = await axios({
+        method: "post",
+        url: `/api/comments/add/${data._id}`,
+        headers: { authorization: localStorage.getItem("token") },
+        data: { commentData: data.comment },
+      });
+      console.log(response);
+      return response.data.comments;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+export const upvoteCommentFn = createAsyncThunk(
+  "comments/upvoteComment",
+  async (data) => {
+    try {
+      const response = await axios({
+        method: "post",
+        url: `/api/comments/upvote/${data.postId}/${data.commentId}`,
+        headers: { authorization: localStorage.getItem("token") },
+      });
+      return response.data.posts;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+export const downvoteCommentFn = createAsyncThunk(
+  "comments/downvoteComment",
+  async (data) => {
+    try {
+      const response = await axios({
+        method: "post",
+        url: `/api/comments/downvote/${data.postId}/${data.commentId}`,
+        headers: { authorization: localStorage.getItem("token") },
+      });
+      return response.data.posts;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+export const getCommentsbyPostIdFn = createAsyncThunk(
+  "comments/getCommentsbyPostId",
+  async (_id) => {
+    try {
+      const response = await axios({
+        method: "get",
+        url: `/api/comments/${_id}`,
+        headers: { authorization: localStorage.getItem("token") },
+      });
+      return response.data.comments;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+const commentsSlice = createSlice({
+  name: "comments",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [postCommentFn.fulfilled]: (state, action) => {
+      state.composeComment = action.payload;
+    },
+    [getCommentsbyPostIdFn.fulfilled]: (state, action) => {
+      state.commentsdata = action.payload;
+    },
+    [downvoteCommentFn.fulfilled]: (state, action) => {
+      state.composeComment = action.payload;
+    },
+    [upvoteCommentFn.fulfilled]: (state, action) => {
+      state.composeComment = action.payload;
+    },
+  },
+});
+export default commentsSlice.reducer;
